ddd948d2e382d33cf17e3619f1b77de9f8af8f9b
Lorraineken <lorrainekupa@gmail.com>
2022-12-03 09:31:51 +0300

    Completed Deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    listHandler(e.target.children[1].value)
+
+  })
+  
+ function listHandler(list){
+  //console.log(list)
+  p=document.createElement('p');
+  p.textContent = list;
+  document.getElementById('tasks').appendChild(p)
+ }
+
+ 
+
 });
+
